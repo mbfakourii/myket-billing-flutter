2b95ecd60458968e63d86c1e642f7c86944cfc24
mbfakourii <mbfakourii@gmail.com>
2025-02-20 22:52:48 +0330

    Remove unused Registrar import from MyketPlugin.kt

--- android/src/main/kotlin/ir/mservices/myketiap/MyketPlugin.kt
+++ android/src/main/kotlin/ir/mservices/myketiap/MyketPlugin.kt
@@ -10,7 +10,6 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
 import ir.myket.billingclient.IabHelper
 import ir.myket.billingclient.util.IabResult
 import ir.myket.billingclient.util.Inventory
